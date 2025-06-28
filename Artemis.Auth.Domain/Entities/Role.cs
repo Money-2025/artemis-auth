37ad9d857b5a808bb75fad56468084626b7524d4
Serkan Angün <angun.serkan2@gmail.com>
2025-06-28 15:52:25 +0300

    update role entity

--- Artemis.Auth.Domain/Entities/Role.cs
+++ Artemis.Auth.Domain/Entities/Role.cs
@@ -9,8 +9,10 @@ public class Role
     public bool IsDeleted { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public Guid? CreatedBy { get; set; }
+    public User? CreatedByUser { get; set; } 
     public DateTimeOffset? ModifiedAt { get; set; }
     public Guid? ModifiedBy { get; set; }
+    public User? ModifiedByUser { get; set; } 
     public int RowVersion { get; set; }
 
     public ICollection<UserRole> UserRoles { get; set; }
